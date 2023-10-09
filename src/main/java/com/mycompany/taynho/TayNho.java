fe0f9b55d3cb22898f20ec5b4a8817b76635ce3b
TRONG NGHIA <TRONG NGHIA@PHANTRONGNGHIA>
2023-10-09 22:18:38 +0700

    Init Project

---
+++ src/main/java/com/mycompany/taynho/TayNho.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taynho;
+
+/**
+ *
+ * @author TRONG NGHIA
+ */
+public class TayNho {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
